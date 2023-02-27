c4853c5d4254fa871ce3a2e944999bfe6f1e9d3b
Julieann Britany <julieanb@il-f4.msk.21-school.ru>
2023-02-27 17:08:29 +0300

    12333

--- 02/main.sh
+++ 02/main.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 echo "HOSTNAME =" $(hostname)
-echo "TIMEZONE =" 
+echo "TIMEZONE =" $(timedatectl | awk '{print $3}' | head -4 | grep '/')
+echo "USER =" $(whoami)
+echo "OS =" $(awk '{print $0}' /etc/issue | head -1)
+echo "DATE =" $(date -R | awk '{print $2, $3, $4, $5}')
+echo "UPTIME =" $(uptime -p | awk '{print $2, $3}')
+echo "UPTIME_SEC =" $(cat /proc/uptime | awk '{print $1, "sec"}')
+echo "IP =" $(netstat -rn | head -4 | tail +4 | awk '{print $1}') #$(hostname -I | awk '{print $1}')
+echo "MASK =" $(netstat -rn | head -4 | tail +4 | awk '{print $3}')
+echo "GATEWAY =" $(netstat -rn | head -4 | tail +4 | awk '{print $2}')
+echo "RAM_TOTAL ="  $(free  |  head -2 | tail +2 | awk '{printf("%.3f", $2/(1024*1024)), "Gb"}')
+echo "RAM_USED =" $(free  |  head -2 | tail +2 | awk '{printf("%.3f", $3/(1024*1024)), "Gb"}')
+echo "RAM_FREE =" $(free  |  head -2 | tail +2 | awk '{printf("%.3f", $4/(1024*1024)), "Gb"}')
+echo "SPACE_ROOT =" $(df / |  head -2 | tail +2 | awk '{printf("%.2f", $2/1024)}, "Mb"')
+echo "SPACE_ROOT_USED =" $(df / |  head -2 | tail +2 | awk '{printf("%.2f", $3/1024)}, "Mb"')
+echo "SPACE_ROOT_FREE =" $(df / |  head -2 | tail +2 | awk '{printf("%.2f", $4/1024)}, "Mb"')
